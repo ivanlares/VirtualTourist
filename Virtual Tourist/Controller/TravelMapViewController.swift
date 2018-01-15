0036aea020d058bfd066bc15e6d8f42266b7756b
ivan lares <ivanlares1@gmail.com>
2018-01-14 18:26:12 -0800

    chore: add travel locations storyboard view

---
+++ Virtual Tourist/Controller/TravelMapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by ivan lares on 1/14/18.
+//  Copyright © 2018 ivan lares. All rights reserved.
+//
+
+import UIKit
+
+/// Represents the main map controller that allows users to drop pins
+class TravelMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
