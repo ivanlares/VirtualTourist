73ea375b4b95b2b3932ddce9c454c63a0a491a77
ivan lares <ivanlares1@gmail.com>
2018-01-17 14:32:32 -0800

    chore: add map view remove all annoations method extension

---
+++ Virtual Tourist/Extensions/MKMapView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  MKMapView+Extension.swift
+//  Virtual Tourist
+//
+//  Created by ivan lares on 1/17/18.
+//  Copyright © 2018 ivan lares. All rights reserved.
+//
+
+import MapKit
+
+extension MKMapView{
+    
+    /// Removes all annotations from self
+    func removeAllAnotations(){
+        
+        removeAnnotations(annotations)
+    }
+}
